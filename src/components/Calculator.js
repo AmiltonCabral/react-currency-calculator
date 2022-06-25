ae39ddbdf651cf0da1f8d5b15ebb7c1720865c11
AmiltonCabral <amilton.cabral@ccc.ufcg.edu.br>
2022-06-25 19:10:42 -0300

    first commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function Calculator() {
+
+    const [dollarRealQuote, setDollarRealQuote] = useState(5.24);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Calculator
---
+++ src/components/Stats.js
@@ -0,0 +1,16 @@
+function Stats() {
+    return (
+        <div className="Stats">
+            <div>
+                <p>1 Dólar vale: </p>
+                <p>5,24 Real</p>
+            </div>
+            <div>
+                <p>Cotação do dia 25 Junho 2022</p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Stats
